f361ee35026e4d4d06dd087b52cebed2c5de2af2
Brett Walker <bdwalker93@yahoo.com>
2017-03-15 17:13:51 -0700

    added truncation to title and heading replacement if no title is there

--- src/java/Models/SearchEngine.java
+++ src/java/Models/SearchEngine.java
@@ -256,16 +256,21 @@ public class SearchEngine {
                         Document d = indexSearcher.doc(docId);
                         
                         String resultTitle = d.get("title"); 
+                        String heading = d.get("heading1"); 
                         String urlToPage = d.get("url");
                         String hitScore = String.valueOf(hits[i].score);
-                        
+//                        
                         if(resultTitle == null)
-                            resultTitle = "No Site Title";
-                        
-                        
+                            if(heading != null)
+                                resultTitle = heading;
+                            else
+                                resultTitle = "No Site Title";
+
+                            
+
                         // Need to use the "//" before the url to show its not relative
                         out.print("<li style='padding-bottom: 10px; list-style-type: none'>" + "<span style='float: left; width: 20px'><span  style='float: right'>" + (i + 1) + ".</span></span>" + 
-                                "<span><a style='text-decoration: none; padding: 20px' href=\"//" + urlToPage + "\">" + resultTitle + "</a> (" + (urlToPage.length() <= 40 ? urlToPage : urlToPage.substring(0, 40) + "...") + ")" + "</span></li>");
+                                "<span><a style='text-decoration: none; padding: 20px' href=\"//" + urlToPage + "\">" + (resultTitle.length() <= 120 ? resultTitle : resultTitle.substring(0, 120) + "...") + "</a> (" + (urlToPage.length() <= 40 ? urlToPage : urlToPage.substring(0, 40) + "...") + ")" + "</span></li>");
                         
                     }
                     
